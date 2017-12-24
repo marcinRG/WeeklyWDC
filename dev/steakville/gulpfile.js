7f816e0b574df912a1d54356464ec25cfe48a5c5
marcinRG <marcin.guszpit@gmail.com>
2017-12-24 14:24:40 +0100

    tools & init

---
+++ dev/steakville/gulpfile.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require('vinyl-source-stream');
+var $ = require('gulp-load-plugins')({lazy: true});
+var sassLint = require('gulp-sass-lint');
+var del = require('del');
+
+var settings = require('./gulp.settings/settings');
+
+//code check
+gulp.task('code-check', function () {
+    return gulp.src(settings.app.allJs)
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'))
+        .pipe($.eslint())
+        .pipe($.eslint.format())
+        .pipe($.eslint.failAfterError());
+});
+
+gulp.task('clean-styles', function (done) {
+    var files = settings.app.cssFolder + '*.css';
+    clean(files, done);
+});
+
+gulp.task('lint-sass', ['clean-styles'], function () {
+    return gulp.src(settings.app.scssStyles)
+        .pipe(sassLint(
+            {
+                configFile: '.sass-lint.yml'
+            }
+        ))
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError())
+});
+
+gulp.task('sass-compile', ['lint-sass'],  function () {
+    msg('Kompilacja plików scss -> css');
+    return gulp.src(settings.app.scssFile)
+        .pipe($.sass().on('error', $.sass.logError))
+        .pipe(gulp.dest(settings.app.cssStyles));
+});
+
+gulp.task('browserify-compil', ['code-check'], function () {
+    return browserify({
+        entries: [settings.app.jsFile],
+        debug: true
+    }).bundle()
+        .pipe(source(settings.app.compiledJs))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('browserify-inject-js', ['browserify-compil'], function () {
+    return gulp.src(settings.app.index)
+        .pipe($.inject(gulp.src(settings.app.compiledJS, {read: false}), {relative: true}))
+        .pipe(gulp.dest(settings.app.client));
+});
+
+gulp.task('help', $.taskListing);
+gulp.task('default', ['help']);
+
+//--functions
+function clean(path, done) {
+    $.util.log('Czyszczenie folderu:' + $.util.colors.blue(path));
+    del(path).then(function () {
+        done();
+    });
+}
+
+gulp.task('sass-watcher', function () {
+    gulp.watch(settings.app.scssStyles, ['sass-compile', 'inject-css']);
+});
+
+gulp.task('inject-css', function () {
+    return gulp.src(settings.app.index)
+        .pipe($.inject(gulp.src(settings.app.cssFile, {read: false}), {relative: true}))
+        .pipe(gulp.dest(settings.app.client));
+});
+
+
+function msg(txt) {
+    $.util.log($.util.colors.blue(txt));
+}
