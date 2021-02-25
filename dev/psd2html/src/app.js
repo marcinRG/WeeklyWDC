a6167ef7b79e8b071e730393685f8a3669959451
marcin <marcin.guszpit@gmail.com>
2021-02-25 23:15:59 +0100

    changes

--- dev/psd2html/src/app.js
+++ dev/psd2html/src/app.js
@@ -1,3 +1,24 @@
 import './style.scss';
 
 console.log('it works');
+
+class InputsInactivity {
+    constructor() {
+        this.timer = 15000;
+        this.handler = null;
+    }
+
+    initialize() {
+
+    }
+
+    resetTimer() {
+
+    }
+
+    setHandlers() {
+
+    }
+
+
+}
